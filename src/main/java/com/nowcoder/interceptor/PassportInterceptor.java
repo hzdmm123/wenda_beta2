9dcdd2d467938d9315472cb08f51f7c66ccd5f45
hzdmm123 <hzdmm123@163.com>
2017-05-10 21:30:52 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/interceptor/PassportInterceptor.java
@@ -0,0 +1,68 @@
+package com.nowcoder.interceptor;
+
+import com.nowcoder.dao.LoginTicketDAO;
+import com.nowcoder.dao.UserDAO;
+import com.nowcoder.model.HostHolder;
+import com.nowcoder.model.LoginTicket;
+import com.nowcoder.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * Created by nowcoder on 2016/7/3.
+ */
+@Component
+public class PassportInterceptor implements HandlerInterceptor {//在这里初始化
+
+    @Autowired
+    private LoginTicketDAO loginTicketDAO;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String ticket = null;
+        if (httpServletRequest.getCookies() != null) {
+            for (Cookie cookie : httpServletRequest.getCookies()) {//在登陆的时候会看cookiel里面是否有ticket
+                if (cookie.getName().equals("ticket")) {
+                    ticket = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (ticket != null) {
+            LoginTicket loginTicket = loginTicketDAO.selectByTicket(ticket);//数据库里面找到ticket
+            if (loginTicket == null || loginTicket.getExpired().before(new Date()) || loginTicket.getStatus() != 0) {
+                return true;//如果没找到或者超过期限了就重新登陆
+            }
+
+            User user = userDAO.selectById(loginTicket.getUserId());//不需要重新登陆就维护一个hostholder
+            hostHolder.setUser(user);//设置到里面
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null && hostHolder.getUser() != null) {
+            modelAndView.addObject("user", hostHolder.getUser());//把user一直放进去
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        hostHolder.clear();//退出的时候
+    }
+}
